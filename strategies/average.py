0f11595fae1c60ca57e5a03cfeb427e7bd7679d4
Isaac Wang <isaac@unify.id>
2018-07-14 15:33:25 -0700

    yaboi

--- strategies/average.py
+++ strategies/average.py
@@ -16,7 +16,7 @@ def average(data, p, test):
     trades = []
     if data['type'] == 'book':
         symbol = data['symbol']
-        margin = 20
+        margin = 10
         if symbol == 'XLK':
             margin = 20
         if test:
