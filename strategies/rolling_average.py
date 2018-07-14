643473e5b773e29272471d4fb172a800c730ec08
Isaac Wang <isaac@unify.id>
2018-07-14 12:03:39 -0700

    dsljf

--- strategies/rolling_average.py
+++ strategies/rolling_average.py
@@ -3,15 +3,15 @@ from collections import defaultdict
 def rolling_average(data):
     read_data(data)
     trades = []
-    if data['TYPE'] == 'book':
-        symbol = data['SYMBOL']
-        bids = data['BUY']
+    if data['type'] == 'book':
+        symbol = data['symbol']
+        bids = data['buy']
         for price, size in bids:
             rolling_average = get_rolling_average(symbol)
             if price > rolling_average and rolling_average != -1:
                 trades.append((symbol, price, size, false))
 
-        asks = data['SELL']
+        asks = data['sell']
         for price, size in asks:
             rolling_average = get_rolling_average(symbol)
             if price < rolling_average and rolling_average != -1:
@@ -26,10 +26,10 @@ WINDOW_SIZE = 4
 MIN_WINDOW_SIZE = 1
 
 def read_data(data):
-    if data['TYPE'] == 'trade':
-        symbol = data['SYMBOL']
-        trade_history[symbol].append((data['PRICE'], data['SIZE']))
-        windows[symbol].append(data['PRICE'])
+    if data['type'] == 'trade':
+        symbol = data['symbol']
+        trade_history[symbol].append((data['price'], data['size']))
+        windows[symbol].append(data['price'])
         if len(windows[symbol]) > WINDOW_SIZE:
             windows[symbol].pop(0)
     print(trade_history)
