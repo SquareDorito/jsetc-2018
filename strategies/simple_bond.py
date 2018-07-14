a892cf8d219aa831bee6af280db60f3be6a48604
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 11:50:10 -0700

    asdfsadf

--- strategies/simple_bond.py
+++ strategies/simple_bond.py
@@ -2,18 +2,17 @@ def simple_bond(data):
     trades = []
     if data['type'] == 'book' and data['symbol'] == 'BOND':
         bids = data['buy']
-        print(f"bids: {data['buy']}")
+        print('bids: ', {data['buy']})
         for price, size in bids:
             if price > 1000:
                 trades.append(('SELL', 'BOND', price, size))
 
         asks = data['sell']
-        print(f"asks: {data['sell']}")
+        print('asks: ', {data['sell']})
         for price, size in asks:
             if price < 1000:
                 trades.append(('BUY', 'BOND', price, size))
 
     if len(trades) > 0:
-        print('trades: ',)
-        print(trades)
+        print('trades: ', trades)
     return trades
