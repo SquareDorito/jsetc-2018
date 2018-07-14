eafdc9c9fd780509fc8e281786a72ca3816b03a0
Isaac Wang <isaac@unify.id>
2018-07-14 15:05:42 -0700

    lower margin if test

--- strategies/average.py
+++ strategies/average.py
@@ -19,6 +19,7 @@ def average(data, p, test):
         symbol = data['symbol']
 
         if test:
+            MARGIN = 10
             average = expAverageDict[symbol] if symbol in expAverageDict else -1
         else:
             average = get_local_average(symbol)
