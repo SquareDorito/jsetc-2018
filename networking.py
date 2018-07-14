3b9cf76e1107d539fe92dcf73e551fea3b51f985
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 12:11:23 -0700

    changes

--- networking.py
+++ networking.py
@@ -18,25 +18,10 @@ import json
 team_name="TEAMJONATHAN"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
-
-# This setting changes which test exchange is connected to.
-# 0 is prod-like
-# 1 is slower
-# 2 is empty
-test_exchange_index=0
-prod_exchange_hostname="production"
-
-port=25000 + (test_exchange_index if test_mode else 0)
-exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
-
 # ~~~~~============== NETWORKING CODE ==============~~~~~
-def connect():
+def connect(hostname, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if test_mode:
-        s.connect((exchange_hostname, port))
-    else:
-        s.connect((prod_exchange_hostname, port))
+    s.connect((hostname, port))
     return s.makefile('rw', 1)
 
 def read_from_exchange(exchange):
@@ -48,8 +33,22 @@ def write_to_exchange(exchange, obj):
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
-def main():
-    exchange = connect()
+def main(test_mode, srv):
+    # This setting changes which test exchange is connected to.
+    # 0 is prod-like
+    # 1 is slower
+    # 2 is empty
+    test_exchange_index=0
+    prod_exchange_hostname="production"
+
+    port=25000 + (test_exchange_index if test_mode else 0)
+
+    exchange = None
+    if test_mode:
+        exchange = connect('test-exch-' + team_name, port)
+    else:
+        exchange = connect('production', 25000)
+    
     write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
     hello_from_exchange = read_from_exchange(exchange)
     # A common mistake people make is to call write_to_exchange() > 1
@@ -67,4 +66,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser('etc')
+    parser.add_argument('--test', action='store_true', default=False)
+    parser.add_argument('--serv', action='store_const', default=0)
+    args = parser.parse_args()
+    main(args.test, args.serv)
