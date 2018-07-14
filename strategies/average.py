8c100edd87ad627e05e82b9df9254d3d19b95373
Isaac Wang <isaac@unify.id>
2018-07-14 16:25:45 -0700

    lmao

--- strategies/average.py
+++ strategies/average.py
@@ -23,9 +23,10 @@ def average(data, p, test):
         # if test and symbol == 'BABA':
         #     margin = 10
         else:
-            average = get_local_average(symbol)
             if symbol == 'BABA':
                 average = get_local_average('BABZ')
+            else:
+                average = get_local_average(symbol)
 
         bids = data['buy']
         for price, size in bids:
