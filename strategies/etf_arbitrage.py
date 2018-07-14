1af69fed3ba76b310f24e9d7a14c65f1cf19b80a
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 14:34:28 -0700

    asdfad

--- strategies/etf_arbitrage.py
+++ strategies/etf_arbitrage.py
@@ -1,8 +1,23 @@
 from .average import read_data
 from collections import defaultdict
 
-averageDict = defaultdict(int)
-totalCountDict = defaultdict(int)
-windowDict = defaultdict(int)
-def etf(data, test):
-    pass
+averageDict = defaultdict(int) # averages from the start
+totalCountDict = defaultdict(int) # number of trades
+windowDict = defaultdict(int) # last n prices
+def etf(data, p, test):
+	valid_symbols = ['xlk', 'goog', 'aapl', 'msft', 'bond']
+	read_data(
+		data, p, test, 
+		averageDict, 
+		totalCountDict, 
+		windowDict, 
+		valid_symbols
+	)
+	trades = []
+	constituents = windowDict['goog'] + windowDict['aapl'] + windowDict['msft'] + 1000
+	
+	if data['type'] == 'book':
+		symbol = data['symbol'].lower()
+		if symbol == 'xlk':
+			return
+			
