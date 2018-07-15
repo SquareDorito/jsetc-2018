6ab37361f316222b46793d4c958dd70b8e35e437
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 18:32:33 -0700

    asddas

--- bot.py
+++ bot.py
@@ -49,8 +49,8 @@ class Bot:
 			'dir': direction,
 			'size': size
 		}
-		#if self.test:
-			#print(order)
+		if self.test:
+			print(order)
 		self.conversions[self.id] = (sym, size, buy)
 		self.write_to_exchange(order)
 		self.id += 1
