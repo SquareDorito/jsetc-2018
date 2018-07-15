06d61fe5bb2baf038e31dde54b6805e1c3c37289
SquareDorito <khnoh@brown.edu>
2018-07-14 18:16:14 -0700

    margin

--- strategies/adr_pair_trading.py
+++ strategies/adr_pair_trading.py
@@ -6,7 +6,7 @@ totalCountDict = defaultdict(int) # number of trades
 windowDict = defaultdict(list) # last n prices
 
 MIN_COUNT_TO_TRADE = 5 # when do we trust our average?
-MARGIN=10
+MARGIN=5
 
 def adr_pair(data, p, test):
 	valid_symbols = ['BABA', 'BABZ']
@@ -22,7 +22,7 @@ def adr_pair(data, p, test):
 	if data['type'] == 'book' and data['symbol'] == 'BABA':
 		bids=data['buy']
 		for price, size in bids:
-			if price>get_local_average('BABZ'):
+			if price>get_local_average('BABZ')+MARGIN:
 				if p.get('babz')<=-10:
 					break
 				temp_size=size if p.get('babz')-size>=-10 else p.get('babz')+10
@@ -32,7 +32,7 @@ def adr_pair(data, p, test):
 
 		asks=data['sell']
 		for price, size in asks:
-			if price<get_local_average('BABZ'):
+			if price<get_local_average('BABZ')-MARGIN:
 				if p.get('babz')>=10:
 					break
 				temp_size=size if p.get('babz')+size<=10 else 10-p.get('babz')
