bcf2d4d2e44d8830fccf89666a5280e8061df01b
SquareDorito <khnoh@brown.edu>
2018-07-14 21:25:29 -0700

    fuckjz

--- strategies/adr_pair_trading.py
+++ strategies/adr_pair_trading.py
@@ -5,8 +5,8 @@ averageDict = defaultdict(int) # averages from the start
 totalCountDict = defaultdict(int) # number of trades
 windowDict = defaultdict(list) # last n prices
 
-MIN_COUNT_TO_TRADE = 5 # when do we trust our average?
-MARGIN=20
+MIN_COUNT_TO_TRADE = 100 # when do we trust our average?
+MARGIN=25
 
 def adr_pair(data, p, test):
 	valid_symbols = ['BABA', 'BABZ']
@@ -22,7 +22,7 @@ def adr_pair(data, p, test):
 	if data['type'] == 'book' and data['symbol'] == 'BABA':
 		bids=data['buy']
 		for price, size in bids:
-			if price>get_local_average('BABZ')+MARGIN:
+			if price>get_local_average('BABZ')+MARGIN and totalCountDict['BABZ'] > total:
 				if p.get('baba')<=-10:
 					break
 				temp_size=size if p.get('baba')-size>=-10 else p.get('baba')+10
@@ -30,7 +30,7 @@ def adr_pair(data, p, test):
 
 		asks=data['sell']
 		for price, size in asks:
-			if price<get_local_average('BABZ')-MARGIN:
+			if price<get_local_average('BABZ')-MARGIN and totalCountDict['BABZ'] > total:
 				if p.get('baba')>=10:
 					break
 				temp_size=size if p.get('baba')+size<=10 else 10-p.get('baba')
