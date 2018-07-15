84111ae630304a34da52c76acaefea95de74db87
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 19:48:45 -0700

    asddas

--- strategies/etf_arbitrage.py
+++ strategies/etf_arbitrage.py
@@ -21,35 +21,36 @@ def etf(data, p, test):
 		return trades
 	constituents = 2 * get_local_average('GOOG', windowDict) + 2 * get_local_average('AAPL', windowDict) + 3 * get_local_average('MSFT', windowDict) + 3 * 1000
 	constituents = constituents / 10
-
-	if data['type'] == 'book':
-		symbol = data['symbol']
-		bids = data['buy']
-		asks = data['sell']
-		if abs(p.diff['xlk']) > 10:
-			diff = 10 * (p.diff['xlk'] // 10)
-			if diff < 0:
-				diff += 10
-			p.diff['goog'] -= 2 * diff
-			p.diff['aapl'] -= 2 * diff
-			p.diff['msft'] -= 3 * diff
-			p.diff['bond'] -= 3 * diff
-			p.diff['xlk'] -= diff
-		
-		if symbol in valid_symbols[1:]:
-			if p.diff[symbol.lower()] != 0:
-				size = p.diff[symbol.lower()]
-				sign = 1 if size > 0 else -1
-				direction = size > 0
-				size = abs(size)
-				
-				market = data['buy' if direction else 'sell']
-				for price, m_size in market:
-					if size == 0:
-						break
-					trades.append((symbol, price, max(size, m_size)))
-					size -= max(size, m_size)
-				p.diff[symbol.lower()] = sign * size
+	if test:
+		if data['type'] == 'book':
+			symbol = data['symbol']
+			bids = data['buy']
+			asks = data['sell']
+			if abs(p.diff['xlk']) > 10:
+				print(p.diff)
+				diff = 10 * (p.diff['xlk'] // 10)
+				if diff < 0:
+					diff += 10
+				p.diff['goog'] -= 2 * diff
+				p.diff['aapl'] -= 2 * diff
+				p.diff['msft'] -= 3 * diff
+				p.diff['bond'] -= 3 * diff
+				p.diff['xlk'] -= diff
+			
+			if symbol in valid_symbols[1:]:
+				if p.diff[symbol.lower()] != 0:
+					size = p.diff[symbol.lower()]
+					sign = 1 if size > 0 else -1
+					direction = size > 0
+					size = abs(size)
+					
+					market = data['buy' if direction else 'sell']
+					for price, m_size in market:
+						if size == 0:
+							break
+						trades.append((symbol, price, max(size, m_size)))
+						size -= max(size, m_size)
+					p.diff[symbol.lower()] = sign * size
 
 		if symbol == 'XLK':
 			for price, size in asks:
