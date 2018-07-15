68044930592fbcab98075af83a8308505ac153c3
Jonathan Zhang <jz2814@columbia.edu>
2018-07-14 20:49:08 -0700

    dont hug me im scared

--- strategies/average.py
+++ strategies/average.py
@@ -93,8 +93,7 @@ def read_data(
 
 
 def get_local_average(symbol, windowDict=windowDict):
-    if test:
-        return expAverageDict[symbol]
+    return expAverageDict[symbol]
     if len(windowDict[symbol]) > 0:
         return 1.0 * sum(windowDict[symbol]) / len(windowDict[symbol])
     return -1
